1cff5ae2282904cca4988beba0e0b91314609195
philipperemy <premy@cogent.co.jp>
2019-06-18 16:23:49 +0900

    1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cond-rnn',
-    version='1.2',
+    version='1.3',
     description='Conditional RNN',
     author='Philippe Remy',
     license='MIT',
