73d58525be1dda11d744aaf9b701aafe3f0a4a9b
Philippe Remy <premy.enseirb@gmail.com>
2020-01-24 20:06:35 +0900

    2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cond-rnn',
-    version='2.0',
+    version='2.1',
     description='Conditional RNN',
     author='Philippe Remy',
     license='MIT',
@@ -10,7 +10,7 @@ setup(
     long_description=open('README.md').read(),
     packages=['cond_rnn'],
     install_requires=[
-        'numpy'
+        'numpy',
         'tensorflow>=2.x',
     ]
 )
