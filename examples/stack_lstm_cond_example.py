26ab927cce5dad2f20fab63cf2d6c3e52de63143
Philippe Remy <premy.enseirb@gmail.com>
2021-01-03 13:12:24 +0100

    CI

--- examples/stack_lstm_cond_example.py
+++ examples/stack_lstm_cond_example.py
@@ -1,8 +1,8 @@
 import numpy as np
 from single_cond_example import create_conditions
+from tensorflow.keras import Input, Model
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, LSTM
-from tensorflow.keras.optimizers import Adam
 
 from cond_rnn import ConditionalRNN
 
@@ -14,27 +14,45 @@ NUM_CELLS = 6
 
 
 def main():
+    # Cannot do stacked ConditionalRNN with Sequential. Have to rely on the functional API. See below.
     model = Sequential(layers=[
         ConditionalRNN(NUM_CELLS, cell='LSTM', return_sequences=True, name='cond_rnn_0'),
         LSTM(NUM_CELLS),
         Dense(units=NUM_CLASSES, activation='softmax')
     ])
 
-    # Define (real) data.
+    # Stacked ConditionalRNN with the functional API.
+    i = Input(shape=[TIME_STEPS, INPUT_DIM], name='input_0')
+    c = Input(shape=[NUM_CLASSES], name='input_1')
+    # add the condition tensor here.
+    x = ConditionalRNN(NUM_CELLS, cell='LSTM', return_sequences=True, name='cond_rnn_0')([i, c])
+    # and here too.
+    x = ConditionalRNN(NUM_CELLS, cell='LSTM', return_sequences=False, name='cond_rnn_1')([x, c])
+    x = Dense(units=NUM_CLASSES, activation='softmax')(x)
+    model2 = Model(inputs=[i, c], outputs=[x])
+
+    # Define (dummy) data.
     train_inputs = np.random.uniform(size=(NUM_SAMPLES, TIME_STEPS, INPUT_DIM))
     test_inputs = np.random.uniform(size=(NUM_SAMPLES, TIME_STEPS, INPUT_DIM))
     test_targets = train_targets = create_conditions()
 
-    model.compile(optimizer=Adam(learning_rate=0.1), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model2.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     model.fit(
         x=[train_inputs, train_targets], y=train_targets,
         validation_data=([test_inputs, test_targets], test_targets),
-        epochs=10
+        epochs=3
     )
 
-    te_loss, te_acc = model.evaluate([test_inputs, test_targets], test_targets)
-    assert abs(te_acc - 1) < 1e-5
+    model2.fit(
+        x=[train_inputs, train_targets], y=train_targets,
+        validation_data=([test_inputs, test_targets], test_targets),
+        epochs=3
+    )
+
+    assert abs(model.evaluate([test_inputs, test_targets], test_targets)[1] - 1) < 1e-5
+    assert abs(model2.evaluate([test_inputs, test_targets], test_targets)[1] - 1) < 1e-5
 
 
 if __name__ == '__main__':
