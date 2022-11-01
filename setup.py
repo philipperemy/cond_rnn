fdf4ebac76560582dd74f85ba7b65c283ddf6d89
philipperemy <premy.enseirb@gmail.com>
2022-11-01 09:26:24 +0700

    3.1

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
+import os
+import platform
+
 from setuptools import setup
+
+tensorflow = 'tensorflow'
+if platform.system() == 'Darwin' and platform.processor() == 'arm':
+    tensorflow = 'tensorflow-macos'
+    # https://github.com/grpc/grpc/issues/25082
+    os.environ['GRPC_PYTHON_BUILD_SYSTEM_OPENSSL'] = '1'
+    os.environ['GRPC_PYTHON_BUILD_SYSTEM_ZLIB'] = '1'
+
+install_requires = ['numpy', tensorflow, 'tensorflow_addons']
 
 setup(
     name='cond-rnn',
-    version='3.0',
+    version='3.1',
     description='Conditional RNN',
     author='Philippe Remy',
     license='MIT',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     packages=['cond_rnn'],
-    install_requires=[
-        'numpy',
-        'tensorflow>=2.3.0',
-        'protobuf<=3.20'
-    ]
+    install_requires=install_requires
 )
